bdb789ecf8252edddb71563be6dd9b4b634855f0
jinkshower <askthem@naver.com>
2023-12-09 16:03:09 +0900

    test(ChangesCalculatorTest): 잔돈 계산 기능 테스트 추가

---
+++ src/test/java/vendingmachine/domain/ChangesCalculatorTest.java
@@ -0,0 +1,30 @@
+package vendingmachine.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static vendingmachine.domain.Coin.COIN_100;
+import static vendingmachine.domain.Coin.COIN_50;
+
+import java.util.EnumMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChangesCalculatorTest {
+
+    private static Map<Coin, Integer> makeMachine() {
+        Map<Coin, Integer> map = new EnumMap<>(Coin.class);
+        map.put(COIN_100, 4);
+        map.put(COIN_50, 1);
+        return map;
+    }
+
+    @DisplayName("남은 금액과 동전으로 잔돈을 반환 한다")
+    @Test
+    void calculateChanges() {
+        ChangesCalculator changesCalculator = new ChangesCalculator(makeMachine(), 500);
+        Map<Coin, Integer> actual = changesCalculator.execute();
+
+        assertThat(actual).isEqualTo(makeMachine());
+    }
+}
