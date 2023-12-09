755cfb427826e8ed8c6b297527933a5a1422576c
jinkshower <askthem@naver.com>
2023-12-09 15:55:39 +0900

    feat(ChangesCalculator): 가장 높은 값부터 소진하는 기능 구현

--- src/main/java/vendingmachine/domain/ChangesCalculator.java
+++ src/main/java/vendingmachine/domain/ChangesCalculator.java
@@ -3,32 +3,41 @@ package vendingmachine.domain;
 import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class ChangesCalculator {
 
     private final Map<Coin, Integer> machineChanges;
     private int userAmount;
 
-    public ChangesCalculator(int userAmount, Map<Coin, Integer> machineChanges) {
-        this.userAmount = userAmount;
+    public ChangesCalculator(Map<Coin, Integer> machineChanges, int userAmount) {
         this.machineChanges = machineChanges;
+        this.userAmount = userAmount;
     }
 
     public Map<Coin, Integer> execute() {
         Map<Coin, Integer> changes = new EnumMap<>(Coin.class);
         while (!isOver()) {
-            Coin currentHighest = findHighestCoin(machineChanges);
+            Coin currentHighest = findHighestCoin();
             changes.put(currentHighest, changes.getOrDefault(currentHighest, 0) + 1);
+            machineChanges.replace(currentHighest, machineChanges.get(currentHighest) - 1);
+            removeZero();
             userAmount -= currentHighest.getAmount();
         }
         return changes;
     }
 
-    private Coin findHighestCoin(Map<Coin, Integer> machineChanges) {
+    private void removeZero() {
+        for (Coin coin: machineChanges.keySet()) {
+            if (machineChanges.get(coin) <= 0) {
+                machineChanges.remove(coin);
+            }
+        }
+    }
+
+    private Coin findHighestCoin() {
         return machineChanges.keySet().stream()
                 .max(Comparator.comparing(Coin::getAmount))
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] rr찾을 수 없습니다."));
 
     }
 
@@ -40,6 +49,9 @@ public class ChangesCalculator {
     }
 
     private boolean isOver() {
+        if (machineChanges.isEmpty()) {
+            return true;
+        }
         return userAmount < findMinimumAmount();
     }
 }
