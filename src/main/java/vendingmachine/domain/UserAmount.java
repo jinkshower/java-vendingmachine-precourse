21a53c691481de8cf1cc7680643cc2b5d6cf306b
jinkshower <askthem@naver.com>
2023-12-09 14:37:59 +0900

    feat(UserAmount): 투입금액 클랙스 생성

---
+++ src/main/java/vendingmachine/domain/UserAmount.java
@@ -0,0 +1,29 @@
+package vendingmachine.domain;
+
+public class UserAmount {
+
+    private int amount;
+
+    public UserAmount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 유효한 금액이 아닙니다.");
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void minus(int decrement) {
+        amount -= decrement;
+    }
+
+    public boolean isLessThan(int amount) {
+        return this.amount < amount;
+    }
+}
